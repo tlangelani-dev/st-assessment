0a7f50c0f52eaee52dbecf2808c039f92a2fcfa1
Tlangelani Mboweni <mbowenit@gmail.com>
2021-10-25 04:59:00 +0200

    setup server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema');
+const PORT = process.env.PORT || 3002;
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphql: true
+}));
+
+app.listen(PORT, () => console.log(`Express server running on port: ${PORT}`));
