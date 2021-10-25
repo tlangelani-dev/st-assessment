8940111c654e24aca1869b7e19434847423d6a80
Tlangelani Mboweni <mbowenit@gmail.com>
2021-10-25 06:24:36 +0200

    setup app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    ApolloClient,
+    ApolloProvider,
+    InMemoryCache
+} from '@apollo/client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const client = new ApolloClient({
+    uri: 'http://localhost:3002',
+    cache: new InMemoryCache
+});
+
+const App = () => {
+    return (
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    )
 }
 
-export default App;
+export default App
+
---
+++ client/src/components/People.tsx
@@ -0,0 +1,9 @@
+const People = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default People
